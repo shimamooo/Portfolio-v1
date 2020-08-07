aecd3912dc7b2cd2cb9d4c23cc2dfde6bbf85065
Anthony Kuang <kuanganthony@gmail.com>
2020-08-07 12:28:13 -0700

    blog section

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
 // STATS
-document.querySelector('#months').textContent = new Date().getMonth() - new Date(2020, 04, 20).getMonth();
+document.querySelector('#months').textContent = new Date().getMonth() - new Date(2020, 05, 01).getMonth();
 
 const repositories = fetch("https://api.github.com/users/AnthonyKuang")
   .then(response => response.json())
   .then(data => data.public_repos)
   .then(repos => document.querySelector('#repositories').textContent = repos);
-
-// MEDIUM
-fetch('https://api.medium.com/v1/me', {
-    method: 'GET',
-    mode: 'no-cors',
-    headers: new Headers(
-      {"Content-Type": "application/json",
-      "Accept": "application/json"}
-    )
-  }
-).then(response => response.json())
- .then(json => console.log(json));
