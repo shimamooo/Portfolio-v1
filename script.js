9ae8f192e0b5eccd31655bd87a49f7e7c9e6eb38
Anthony Kuang <kuanganthony@gmail.com>
2020-08-08 13:57:21 -0700

    finished animation

--- script.js
+++ script.js
@@ -5,3 +5,53 @@ const repositories = fetch("https://api.github.com/users/AnthonyKuang")
   .then(response => response.json())
   .then(data => data.public_repos)
   .then(repos => document.querySelector('#repositories').textContent = repos);
+
+// TYPING ANIMATION
+const TypeWriter = function(txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = '';
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+}
+
+TypeWriter.prototype.type = function() {
+  const current = this.wordIndex % this.words.length;
+  const fullTxt = this.words[current];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.txtElement.innerHTML = `<span class="showcase__txt">${this.txt}</span>`;
+
+  let typeSpeed = 150;
+
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    typeSpeed = this.wait;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.wordIndex++;
+    typeSpeed = 500;
+  }
+
+  setTimeout(() => this.type(), typeSpeed)
+}
+
+function init() {
+  const txtElement = document.querySelector('.showcase__type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  new TypeWriter(txtElement, words, wait);
+}
+
+document.addEventListener('DOMContentLoaded', init);
