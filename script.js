d15d8652bcab8db5da8b9ce5cd6a1403653bde99
Anthony Kuang <kuanganthony@gmail.com>
2021-01-25 18:55:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// STATS
-document.querySelector('#months').textContent = new Date().getMonth() - new Date(2020, 05, 01).getMonth();
-
 const repositories = fetch("https://api.github.com/users/AnthonyKuang")
   .then(response => response.json())
   .then(data => data.public_repos)
